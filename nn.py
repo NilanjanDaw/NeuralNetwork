37bc950b19713879d95405d4944637e0116e472f
nilanjandaw <nilanjandaw@gmail.com>
2018-10-19 21:41:07 +0530

    initial commit

---
+++ nn.py
@@ -0,0 +1,7 @@
+# @Author: nilanjan
+# @Date:   2018-10-19T21:40:19+05:30
+# @Email:  nilanjandaw@gmail.com
+# @Filename: nn.py
+# @Last modified by:   nilanjan
+# @Last modified time: 2018-10-19T21:40:26+05:30
+# @Copyright: Nilanjan Daw
