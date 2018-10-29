a00a9a6fd3b8b5d2b5e216dcd5bdcdbfbcee011f
nilanjandaw <nilanjandaw@gmail.com>
2018-10-30 03:39:18 +0530

    tanh activation, backprop rewrite

--- nn.py
+++ nn.py
@@ -3,7 +3,7 @@
 # @Email:  nilanjandaw@gmail.com
 # @Filename: nn.py
 # @Last modified by:   nilanjan
-# @Last modified time: 2018-10-29T01:43:37+05:30
+# @Last modified time: 2018-10-30T03:39:01+05:30
 # @Copyright: Nilanjan Daw
 import numpy as np
 import pandas as pd
@@ -13,12 +13,15 @@ import os
 import csv
 
 iterations = 1000
-learning_rate = 0.0001
+learning_rate = 0.001
 lambda_regularizer = 4500
+activation_function = "sigmoid"
+
 
 num_hidden_layers = 1
 num_hidden_nodes = 100
 num_classes = 3
+num_weights = 0
 
 error_rate = 0.00000001
 batch_size = 100
@@ -112,9 +115,25 @@ def sigmoid_dx(data):
     return data * (1 - data)
 
 
-def cross_entropy(predicted, true_label):
-    loss = np.nan_to_num(true_label * np.log(predicted)
-                         + (1 - true_label) * np.log(1 - predicted))
+def tanh(data):
+    return np.tanh(data)
+
+
+def tanh_dx(data):
+    return 1 - (np.tanh(data) ** 2)
+
+
+def cross_entropy(predicted, true_label, layers):
+    global lambda_regularizer
+    loss = np.nan_to_num(true_label * np.log(predicted + epsilon)
+                         + (1 - true_label) * np.log(1 - predicted) + epsilon)
+    if lambda_regularizer is not None:
+        sum_weight = []
+        for layer in layers:
+            sum_weight.append(np.mean(layer ** 2))
+        # print(sum_weight)
+        loss += lambda_regularizer * \
+            np.mean(np.asarray(sum_weight)) / num_weights
     return np.mean(loss)
 
 
@@ -125,8 +144,14 @@ def del_output(predicted, true_label):
 def update_weight(layers, delta_loss, activations):
     global learning_rate
     for index in reversed(range(len(layers))):
-        del_error = np.dot(delta_loss[index], np.asarray(activations[index]))
-        del_error = learning_rate * np.array(del_error)
+        del_error = np.zeros(np.shape(delta_loss[0][index]))
+        for i in range(batch_size):
+            del_error = np.add(del_error, np.dot(delta_loss[i][index], np.asarray(activations[i][index])))
+        # ps("de", del_error)
+        del_error = learning_rate * np.array(del_error) / batch_size
+        if lambda_regularizer is not None:
+            layers[index] *= (1 - learning_rate * lambda_regularizer
+                              / num_weights)
         layers[index] -= del_error.T
     return layers
 
@@ -139,7 +164,10 @@ def forward_pass(activations, layers):
 
     for index in range(len(layers)):
         x = np.dot(activations[index], layers[index])
-        x = sigmoid(x)
+        if activation_function == "tanh" and index < len(layers) - 1:
+            x = tanh(x)
+        else:
+            x = sigmoid(x)
         activations.append(x)
 
     return activations
@@ -156,10 +184,13 @@ def evaluate(layers, x, y=None):
 
     for index in range(len(layers)):
         x = np.dot(x, layers[index])
-        x = sigmoid(x)
+        if activation_function == "tanh" and index < len(layers) - 1:
+            x = tanh(x)
+        else:
+            x = sigmoid(x)
 
     if y is not None:
-        cross_entropy_loss = cross_entropy(x, y)
+        cross_entropy_loss = cross_entropy(x, y, layers)
         accuracy = accuracy_calculate(x, y)
         return x, -cross_entropy_loss, accuracy
     return x
@@ -173,21 +204,19 @@ def back_propagation(x, true_label, layers, bias):
 
     activations = forward_pass(activations, layers)
 
-    for index in range(len(activations)):
-        activations[index] = np.squeeze(activations[index])
-
-    cross_entropy_loss = cross_entropy(activations[-1], true_label)
-
     delta_loss.insert(0, del_output(activations[-1], true_label).T)
     for index in reversed(range(len(layers) - 1)):
-        del_sigmoid = sigmoid_dx(activations[index + 1])
+        if activation_function == "tanh":
+            del_sigmoid = tanh_dx(activations[index + 1])
+        else:
+            del_sigmoid = sigmoid_dx(activations[index + 1])
         hidden_layer_loss = np.dot(np.asmatrix(
             layers[index + 1]), delta_loss[0])
         hidden_layer_loss = hidden_layer_loss * del_sigmoid
+        hidden_layer_loss = np.sum(hidden_layer_loss, axis=1)
         delta_loss.insert(0, hidden_layer_loss)
 
-    layers = update_weight(layers, delta_loss, activations)
-    return -cross_entropy_loss, layers
+    return layers, activations, delta_loss
 
 
 def unison_shuffled(a, b):
@@ -202,20 +231,23 @@ def plot_fig(data, label):
     plt.show()
 
 
-def adagrad(loss_validate, validation_loss):
+def decay(loss, min_error):
     global learning_rate
-    decay_rate = 0.01
-    loss_validate = np.asarray(loss_validate)
-    loss_validate = decay_rate * np.sum(validation_loss ** 2)
-    learning_rate += - learning_rate * validation_loss / (np.sqrt(loss_validate) + 0.000001)
+    if min_error is None:
+        return loss
+    if loss < min_error:
+        learning_rate /= (1 + np.abs(min_error - loss))
+        min_error = loss
+    return min_error
 
 
 def train_network(x_train, y_train, x_validate, y_validate):
-
+    global num_weights
     print(num_hidden_layers)
     input_layer, hidden_layers, output_layer, bias_hidden, bias_output = \
         init(np.shape(x_train[0])[0],
              num_hidden_layers, num_hidden_nodes, num_classes)
+    min_error = None
 
     print(np.shape(input_layer), np.shape(
         hidden_layers), np.shape(output_layer))
@@ -228,23 +260,51 @@ def train_network(x_train, y_train, x_validate, y_validate):
     layers.append(output_layer)
     bias.append(bias_hidden)
     bias.append(bias_output)
+
+    num_weights = input_layer.size + hidden_layers.size + output_layer.size
+
     loss_train = []
+    acc_train = []
     loss_validate = []
     acc_validate = []
     print(np.shape(layers))
     for i in range(iterations):
+        delta_loss = []
+        activations = []
         x_train, y_train = unison_shuffled(x_train, y_train)
-        loss_train_i, layers = back_propagation(
-            x_train[0:batch_size, :], y_train[0:batch_size, :], layers, bias)
-        print("Iteration", i, "loss", loss_train_i)
 
-        loss_train.append(loss_train_i)
+        x = x_train[0:batch_size, :]
+        y = y_train[0:batch_size, :]
+
+        for index in range(len(x)):
+            layers, activation_i, delta_loss_i = back_propagation([x[index]], [y[index]], layers, bias)
+            delta_loss.append(delta_loss_i)
+            activations.append(activation_i)
+        layers = update_weight(layers, delta_loss, activations)
         eval, validation_loss, validation_acc = \
             evaluate(layers, x_validate, y_validate)
+        eval, train_loss, train_acc = \
+            evaluate(layers, x_train[0:batch_size, :],
+                     y_train[0:batch_size, :])
         loss_validate.append(validation_loss)
         acc_validate.append(validation_acc)
+        loss_train.append(train_loss)
+        acc_train.append(train_acc)
+        min_error = decay(validation_loss, min_error)
+        if i % 20 == 0:
+            print("Iteration", i, "loss", train_loss, "accuracy", train_acc,
+                  "learning_rate", learning_rate)
         # adagrad(loss_validate, validation_loss)
+    eval, train_loss, train_acc = \
+        evaluate(layers, x_train, y_train)
+    print("mean train loss: ", np.mean(loss_train),  "mean train accuracy: ",
+          np.mean(acc_train))
+    print("mean train loss (whole set): ", train_loss,  "mean train accuracy: ",
+          train_acc)
+    print("mean validation loss: ", np.mean(loss_validate),  "mean validation accuracy: ",
+          np.mean(acc_validate))
     plot_fig(loss_train, label="train_loss")
+    plot_fig(acc_train, label="test_acc")
     plot_fig(acc_validate, label="validation_acc")
     plot_fig(loss_validate, label="validation_loss")
     return loss_train, layers
@@ -261,21 +321,30 @@ def test(x_test, layers):
 
 
 def main():
-    global learning_rate, lambda_regularizer, num_hidden_layers, num_hidden_nodes
+    global learning_rate, lambda_regularizer, num_hidden_layers, \
+        num_hidden_nodes, batch_size, activation_function
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--lambda', dest='lambda_rate', type=float)
     parser.add_argument('--alpha', dest='learning_rate', type=float)
+    parser.add_argument('--activation', dest='activation', type=str)
     parser.add_argument('--num_hidden_layers',
                         dest='num_hidden_layers', type=float)
     parser.add_argument('--num_hidden_nodes',
                         dest='num_hidden_nodes', type=float)
+    parser.add_argument('--batch',
+                        dest='batch_size', type=float)
     parser.add_argument('--train', dest='train', type=str)
     parser.add_argument('--test', dest='test', type=str)
+
     args = parser.parse_args()
 
     learning_rate = args.learning_rate if args.learning_rate is not None \
         else learning_rate
+    batch_size = int(args.batch_size) if args.batch_size is not None \
+        else batch_size
+    activation_function = args.activation if args.activation is not None \
+        else activation_function
     lambda_regularizer = args.lambda_rate
     num_hidden_layers = int(args.num_hidden_layers)
     num_hidden_nodes = int(args.num_hidden_nodes)
